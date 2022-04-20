34af3599a604127a7c11a5dbe3f9e7a93ee0788e
matte887 <m.erroi92@gmail.com>
2022-04-20 17:26:23 +0200

    Snack 3: aggiunto controllo su input

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,7 +1,14 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
 
 // Chiedo all'utente un numero e lo trasformo subito da stringa a numero.
-const userNumber = parseInt(prompt("Dimmi un numero."));
+
+// Creo una variabile per il numero che l'utente inserirà
+let userNumber;
+
+// Creo un ciclo che chiederà un numero all'utente finché questi non inserirà un numero (e non una stringa).
+do {
+    userNumber = parseInt(prompt("Dimmi un numero."));
+} while (isNaN(userNumber));
 
 // Creo un array che conterrà tutti i risultati.
 let result = [];
@@ -9,10 +16,11 @@ let result = [];
 // Creo un contatore per il ciclo while
 let counter = 1;
 
-// Creo il ciclo while che fa un'operazione un numero di volte pari al numero inserito dall'utente.
+// Creo il ciclo while che fa l'operazione del cubo un numero di volte pari al numero inserito dall'utente...
 while (counter <= userNumber) {
-    // Pusho il risultato dentro l'array vuoto.
-    result.push(counter * counter * counter);
+    // ...e pusho il risultato dentro l'array vuoto.
+    result.push(`Il cubo di ${counter} è ${counter * counter * counter}`);
+    // Faccio avanzare il ciclo
     counter++;
 }
 
